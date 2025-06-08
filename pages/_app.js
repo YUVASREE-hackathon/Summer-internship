0f4696b4f0ab42874c580bd42fe127aad8784da5
YUVASREE-hackathon <71762307061@cit.edu.in>
2025-06-08 16:57:15 +0530

    Fix CSS import path

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import "@/styles/globals.css";
+import "../styles/globals.css"; // ✅ Safe and universal
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
